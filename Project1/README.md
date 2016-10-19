400e6daf49d462fdf2bf11175a6644b0116f48fd
Yigit Polat <yigit.polat@ug.bilkent.edu.tr>
2016-10-19 09:03:50 -0700

    modified modpcb; now takes arguments.
    modpcb can show a specific process info with its pid.

--- Project1/README.md
+++ Project1/README.md
@@ -20,7 +20,7 @@ _modify it as you wish_
 
 ### SIMPLE KERNEL MODULE
 **\# make** //compile kernel module  
-**\# insmod modpcb.ko** //to insert our lovely module  
+**\# insmod modpcb.ko param_pid=XXX** //to insert our lovely module and specify a process id to inspect, (change XXX)   
 **\# dmesg | tail** //to see what our lovely kernel module says when inserted  
 **\# rmmod modpcb** //lets remove it from the kernel  
 **\# dmesg | tail** //to see what our cute kernel module says when leaving us :(  
--- Project1/modpcb.c
+++ Project1/modpcb.c
@@ -1,31 +1,52 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/sched.h> //task_pid_nr
+#include <linux/sched.h> 
+#include <linux/moduleparam.h>
+
+static int param_pid = 0;
 
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
-       printk(KERN_INFO "Loading Module\n");
-       printk("The process id is %d\n", (int) task_pid_nr(current));
-       printk("The process vid is %d\n", (int) task_pid_vnr(current));
-       printk("The process name is %s\n", current->comm);
-       printk("The process tty is %d\n", current->signal->tty);
-       printk("The process group is %d\n", (int) task_tgid_nr(current));
-       printk("\n\n");
-   //return -1; //debug mode of working
-   return 0;
+	struct task_struct *cur;
+
+	#define next_task(p) \
+	list_entry_rcu((p)->tasks.next, struct task_struct, tasks)
+
+	#define for_each_process(p) \
+	for (p = &init_task ; (p = next_task(p)) != &init_task ; )
+
+	rcu_read_lock();                                                    
+	for_each_process(cur) {                                             
+		task_lock(cur);                                             
+		if((int) task_pid_nr(cur) == param_pid){
+			printk(KERN_INFO "Loading Module\n");
+			printk("ID: %d\n", (int) task_pid_nr(cur));
+			printk("NAME: %s\n", cur->comm);
+			printk("GROUP: %d\n", (int) task_tgid_nr(cur));
+			printk("PRIO: %ld\n", (long int) cur->prio);
+			printk("VID: %d\n", (int) task_pid_vnr(cur));
+			printk("TTY %d\n", cur->signal->tty);
+			printk("------------\n------------\n");
+		}
+		task_unlock(cur);                                           
+	}                                                                    
+	rcu_read_unlock();       
+
+	return 0; //leave.
 }
 
 /* This function is called when the module is removed. */
 void simple_exit(void) {
-    printk(KERN_INFO "Removing Module\n");
+	printk(KERN_INFO "Removing Module, Bye\n");
 }
 
 /* Macros for registering module entry and exit points. */
 module_init( simple_init );
 module_exit( simple_exit );
+module_param( param_pid, int, 0);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Module");
-MODULE_AUTHOR("SGG");
+MODULE_AUTHOR("yigit");

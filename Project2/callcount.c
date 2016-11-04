d319b06527c0c0cf4f19b3915095f30191ba46f4
Yigit Polat <yigit.polat@ug.bilkent.edu.tr>
2016-11-04 11:19:22 +0300

    reduce done.

--- Project2/callcount.c
+++ Project2/callcount.c
@@ -14,10 +14,39 @@ struct caller
   long count;
 };
 
+struct call
+{
+  u64 callerID;
+  u64 calledID;
+  u64 year;
+};
+
+struct callList
+{
+  u64 callerID;
+  u64 calledID;
+  u64 year;
+  struct callList* next;
+};
+
+struct callerList
+{
+  u64 id;
+  long count;
+  struct callerList* next;
+};
+
+struct calledList
+{
+  u64 id;
+  struct calledList* next;
+};
+
 /*
 Global variables here
 */
 FILE** temp_files;
+FILE** temp_files_out;
 int N;
 int R;
 char** inFileNames;
@@ -33,6 +62,131 @@ void mapper();
 void reducer();
 void* mapperThread( int i);
 void* reducerThread( int i);
+struct caller* countCallers( struct call* call_arr, long count, long* reduced_count);
+struct callList* appendToList( FILE* fp, struct callList* myList, long* count);
+struct call* sortByCallerId( struct callList* myList, long count);
+
+int compareCalls( const void* a, const void* b)
+{
+  u64 callerA;
+  u64 callerB;
+  struct call callA;
+  struct call callB;
+
+  callA = *( struct call*)a;
+  callB = *( struct call*)b;
+  callerA = callA.callerID;
+  callerB = callB.callerID;
+  if( callerA > callerB)
+    return 1;
+  else if( callerA == callerB)
+    return 0;
+  else
+    return -1;
+}
+
+struct call* sortByCallerId( struct callList* myList, long count)
+{
+  struct callList* curList;
+  struct call* sorted_arr;
+  long i;
+
+  sorted_arr = (struct call*) malloc( sizeof( struct call) * count);
+  curList = myList;
+  for( i = 0; i < count; i++)
+  {
+    sorted_arr[i].callerID = curList->callerID;
+    sorted_arr[i].calledID = curList->calledID;
+    sorted_arr[i].year = curList->year;
+    curList = curList->next;
+  }
+
+  qsort( sorted_arr, count, sizeof( struct call), compareCalls);
+  return sorted_arr;
+}
+
+struct callList* appendToList( FILE* fp, struct callList* myList, long* count)
+{
+  u64 callerID;
+  u64 calledID;
+  u64 year;
+  struct callList* curList;
+
+  while( EOF != fscanf(fp, "%llu %llu %llu\n", &callerID, &calledID, &year))
+  {
+    if( callerID <= endID && callerID >= startID && year <= endYear && year >= startYear)
+    {
+      curList = (struct callList*) malloc( sizeof( struct callList));
+      curList->callerID = callerID;
+      curList->calledID = calledID;
+      curList->year = year;
+      curList->next = myList;
+      myList = curList;
+      (*count)++;
+    }
+  }
+
+  return myList;
+}
+
+struct caller* countCallers( struct call* call_arr, long count, long* reduced_count)
+{
+  struct caller* reduced_callers;
+  struct callerList* curList;
+  struct callerList* myList;
+  long newcount;
+  long i, j, last, numb;
+  int unique;
+
+  //initialize for the first elem.
+  myList = (struct callerList*) malloc( sizeof( struct callerList));
+  myList->id = (count ? call_arr[0].callerID : 0);
+  myList->count = 0;
+  myList->next = 0;
+
+  newcount = (count ? 1 : 0);
+  last = 0;
+  for( i = 0; i < count; i++)
+  {
+    if( call_arr[i].callerID != call_arr[last].callerID)
+    {
+      last = i;
+      curList = (struct callerList*) malloc( sizeof( struct callerList));
+      curList->id = call_arr[i].callerID;
+      curList->count = 0;
+      curList->next = myList;
+      myList = curList;
+      newcount++;
+    }
+
+    unique = 1;
+    for( j = last; j < i; j++) //bigOh(individuals_avg_call_count^2) but very fast loop.
+    {
+      if( call_arr[i].calledID == call_arr[j].calledID)
+      {
+        unique = 0;
+        break;
+      }
+    }
+
+    if( !(myList->count) || ( unique))
+      myList->count++;
+  }
+
+  reduced_callers = ( struct caller*) malloc( sizeof( struct caller) * newcount);
+  curList = myList;
+  for( i = 0; i < newcount; i++)
+  {
+    reduced_callers[i].id = curList->id;
+    reduced_callers[i].count = curList->count;
+    curList = curList->next;
+  }
+
+  *reduced_count = newcount;
+  return reduced_callers;
+}
+
+
 
 void* mapperThread( int i)
 {
@@ -41,7 +195,7 @@ void* mapperThread( int i)
   while( EOF != fscanf(infiles[i], "%llu %llu %llu\n", &callerID, &calledID, &year))
   {
     //printf("caller:%llu -- called:%llu -- year:%llu\n", callerID, calledID, year);
-    fprintf( temp_files[i + callerID % R], "%llu %llu %llu\n", callerID, calledID, year);
+    fprintf( temp_files[i*R + callerID % R], "%llu %llu %llu\n", callerID, calledID, year);
   }
 
   pthread_exit(0);
@@ -49,11 +203,29 @@ void* mapperThread( int i)
 
 void* reducerThread( int p)
 {
+  char tmp_f_name[256];
+  struct callList* myList;
+  struct call* sorted_arr;
+  struct caller* reduced_callers;
   int i;
+  long count, reduced_count;
+
+  count = 0;
+  myList = 0;
   for( i = 0; i < N; i++)
   {
-    //do for...
-    temp_files[i*R + p];
+    myList = appendToList( temp_files[i*R + p], myList, &count);
+  }
+
+  sorted_arr = sortByCallerId( myList, count);
+  reduced_callers = countCallers( sorted_arr, count, &reduced_count);
+
+  sprintf( tmp_f_name, "temp-out-%d", p );
+  temp_files_out[p] = fopen( tmp_f_name, "w");
+
+  for( i = 0; i < reduced_count; i++)
+  {
+    fprintf(temp_files_out[p], "%llu %ld\n", reduced_callers[i].id, reduced_callers[i].count);
   }
 
   pthread_exit(0);
@@ -63,19 +235,20 @@ void mapper()
 {
   pthread_t* tids;
   char tmp_f_name[256];
-  int i;
+  u64 i;
 
   temp_files = (FILE**) malloc( sizeof(FILE*) * N * R);
+  temp_files_out = (FILE**) malloc( sizeof(FILE*) * N);
   for( i = 0; i < N*R; i++)
   {
-    sprintf(tmp_f_name, "temp%d-%d", i/R, i%R );
-    temp_files[i] = fopen(tmp_f_name, "w");
+    sprintf( tmp_f_name, "temp%llu-%llu", i/R, i%R );
+    temp_files[i] = fopen( tmp_f_name, "w");
   }
 
   tids = (pthread_t*) malloc(sizeof(pthread_t) * N);
   for( i = 0; i < N; i++)
   {
-    pthread_create( &(tids[i]), 0, &mapperThread, (void*) i);
+    pthread_create( &(tids[i]), 0, (void*) &mapperThread, (void*) i);
   }
 
   for( i = 0; i < N; i++)
@@ -83,17 +256,23 @@ void mapper()
     pthread_join( tids[i], 0);
   }
 
+  for( i = 0; i < N*R; i++)
+  {
+    sprintf( tmp_f_name, "temp%llu-%llu", i/R, i%R );
+    freopen( tmp_f_name, "r", temp_files[i]);
+  }
+
 }
 
 void reducer()
 {
   pthread_t* tids;
-  int i;
+  u64 i;
 
-  tids = (pthread_t*) malloc(sizeof(pthread_t) * N);
+  tids = (pthread_t*) malloc(sizeof(pthread_t) * R);
   for( i = 0; i < R; i++)
   {
-    pthread_create( &(tids[i]), 0, &mapperThread, (void*) i);
+    pthread_create( &(tids[i]), 0, (void*) &reducerThread, (void*) i);
   }
 
   for( i = 0; i < R; i++)
@@ -167,9 +346,11 @@ int main( int argc, char *argv[])
 
   //do reduce
   mapper();
+  printf( "mapper() done\n");
 
   //fire the workers
-  //reducer();
+  reducer();
+  printf( "reducer() done\n");
 
 
   //out file...
